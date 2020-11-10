8f5425fabae19cf703f664262d57f50c815bbf4d
Qian(Chloe) Yu <71596572+Qian-Yu-orcallmeChloe@users.noreply.github.com>
2020-11-09 23:49:58 -0500

    update code

--- Order and Deliver/# 607. Sales Person.sql
+++ Order and Deliver/# 607. Sales Person.sql
@@ -1,6 +1,8 @@
 # 607. Sales Person
 
 # Output all the names in the table salesperson, who didn’t have sales to company 'RED'
+# Don't forgot to use DISTINCT to avoid duplicate
+
 
 SELECT DISTINCT s.name AS name 
 FROM salesperson s 
@@ -9,3 +11,10 @@ WHERE s.sales_id NOT IN (
     FROM orders o 
     JOIN company c ON o.com_id = c.com_id WHERE c.name = "RED")
 
+# or 
+# Will join three table in a row is easier to understand
+SELECT DISTINCT p.name AS name
+FROM salesperson p LEFT JOIN orders o USING sales_id 
+LEFT JOIN company c USING com_id
+WHERE c.name != “RED”
+;
--- Schools and Organizations/# 1350. Students With Invalid Departments.sql
+++ Schools and Organizations/# 1350. Students With Invalid Departments.sql
@@ -1,7 +1,16 @@
 # 1350. Students With Invalid Departments
-
+# Find the id and the name of all students who are enrolled in departments that no longer exists
 SELECT s.id, s.name 
 FROM Students s 
+# Because the SELECT information is from Student table
     LEFT JOIN Departments d ON d.id = s.department_id
 WHERE d.name IS NULL
+;
+
+# or
+# Has to use LEFT JOIN?
+SELECT s.id, s.name
+FROM department d, students s
+WHERE d.id = s.department_id
+AND d.name IS NULL 
 ;
