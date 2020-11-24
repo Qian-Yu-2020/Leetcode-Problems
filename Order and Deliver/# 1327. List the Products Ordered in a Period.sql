0c303a88d3ff682d9f3dafd26e6a60ba0ef0342e
Qian(Chloe) Yu <71596572+Qian-Yu-orcallmeChloe@users.noreply.github.com>
2020-11-24 01:56:41 -0500

    Update # 1327. List the Products Ordered in a Period.sql

--- Order and Deliver/# 1327. List the Products Ordered in a Period.sql
+++ Order and Deliver/# 1327. List the Products Ordered in a Period.sql
@@ -5,7 +5,7 @@
 SELECT product_name, SUM(unit) AS unit
 FROM Orders 
     JOIN Products USING(product_id)
-WHERE LEFT(order_date, 7) = "2020-02"
+WHERE order_date between "2020-02-01" AND "2020-02-29"
 GROUP BY 1
 HAVING SUM(unit) >= 100;
 
