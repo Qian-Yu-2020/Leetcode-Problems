f496a27f90f93a367a2d9157f2da2d20f70ecd67
Qian(Chloe) Yu <71596572+Qian-Yu-2020@users.noreply.github.com>
2020-12-10 23:55:01 -0500

    Update # 1303. Find the Team Size.sql

--- Schools and Organizations/# 1303. Find the Team Size.sql
+++ Schools and Organizations/# 1303. Find the Team Size.sql
@@ -15,8 +15,8 @@ FROM Employee e
 ;
 
 
-
-# Use LEFT JOIN to self-join; Don't forgot to use GROUP BY 
+# Use SELF-JOIN; Don't forgot to use GROUP BY 
+# Why need 2 tables - It's basically used where there is any relationship between rows stored in the same table (correlate pairs of rows from the same table)
 select e1.employee_id,count(*) team_size
 		from employee e1 left join employee e2on e1.team_id = e2.team_id
 group by e1.employee_id;
